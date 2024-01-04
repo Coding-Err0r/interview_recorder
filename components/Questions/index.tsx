e383d4492a3f303d31e9692ad9acbd8560695096
Coding-Err0rr <coding.errorr@gmail.com>
2024-01-04 13:00:38 +0600

    Feature : Questions added

--- components/Questions/index.tsx
+++ components/Questions/index.tsx
@@ -3,6 +3,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { Delay } from "./Delay";
 import { data } from "@/config/data";
+import useMultistepForm from "@/hooks/useMultistepForm";
 
 const motionProps = {
   initial: { opacity: 0 },
@@ -15,25 +16,39 @@ const motionProps = {
 };
 
 const Questions = () => {
+  const { steps, currentStepIndex, step, next, goTo, isLastStep } =
+    useMultistepForm([]);
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <h1 className="text-3xl font-bold text-center text-gray-700">
+    <div className="relative flex flex-col gap-4 p-4">
+      <h1 className="relative text-3xl font-bold text-center text-gray-700">
         Interview Questions
       </h1>
-      <AnimatePresence>
-        {data.questions.map((question: any, index: number) => (
-          <Delay delay={index * 400} key={index}>
-            <motion.div
-              key={index}
-              {...motionProps}
-              className="flex justify-between w-full p-4 bg-white rounded-lg shadow-xl"
-            >
-              <p>{question.question}</p>
-              <p className="text-sm text-emerald-600">{question.time}</p>
-            </motion.div>
-          </Delay>
-        ))}
-      </AnimatePresence>
+      <p className="absolute pb-6 text-xl right-6 top-12">
+        Step {currentStepIndex + 1}/{steps.length}
+      </p>
+      <div className="relative flex flex-col gap-4 mt-6">
+        {step}
+        <AnimatePresence>
+          {data.questions.map((question: any, index: number) => (
+            <Delay delay={index * 400} key={index}>
+              <motion.div
+                key={index}
+                {...motionProps}
+                className="flex justify-between w-full p-4 bg-white rounded-lg shadow-xl "
+              >
+                <p>{question.question}</p>
+                <p className="text-sm text-emerald-600">{question.time}</p>
+              </motion.div>
+            </Delay>
+          ))}
+        </AnimatePresence>
+      </div>
+      <button
+        className="p-4 text-xl font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700"
+        onClick={next}
+      >
+        {isLastStep ? "Finish" : "Next"}
+      </button>
     </div>
   );
 };
---
+++ hooks/useMultistepForm.ts
@@ -0,0 +1,35 @@
+import { ReactElement, useState } from "react";
+
+const useMultistepForm = (steps: ReactElement[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const next = () => {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  };
+
+  const back = () => {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  };
+
+  const goTo = (index: number) => {
+    setCurrentStepIndex(index);
+  };
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    isLastStep : currentStepIndex === steps.length - 1,
+    goTo,
+    back,
+    next,
+    steps
+  };
+};
+
+export default useMultistepForm;
